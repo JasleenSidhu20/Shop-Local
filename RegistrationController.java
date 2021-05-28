acbad90bb7f270bfe1370720c1be79786349b76c
megnabb2 <megnabb2@illinois.edu>
2021-05-28 12:09:55 -0700

    updated scene builders, added registration files

---
+++ RegistrationController.java
@@ -0,0 +1,30 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class RegistrationController {
+    //Small Business Information
+    @FXML
+    private TextField businessNameField;
+
+    @FXML
+    private TextField addressField;
+
+    @FXML
+    private TextField cityField;
+
+    @FXML
+    private TextField stateField;
+
+    @FXML
+    private TextField zipcodeField;
+
+    @FXML
+    private TextField ownerNameField;
+
+    @FXML
+    private TextField emailField;
+
+    @FXML
+    private TextField phoneNumberField;
+
+}
