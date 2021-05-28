a36577e113b563c26f1634efde0c4c7825a25741
megnabb2 <megnabb2@illinois.edu>
2021-05-28 12:42:25 -0700

    updated registration fxml, controller, and changed format of ShopLocal.java (doesnt work)

--- ShopLocal.java
+++ ShopLocal.java
@@ -7,16 +7,33 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
+import java.io.IOException;
+
 public class ShopLocal extends Application {
+    private Stage primaryStage;
+    private BorderPane mainLayout;
     public static void main(String[] args){
         launch(args);
     }
+    
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("AppScene.fxml"));
+        this.primaryStage = primaryStage;
+        this.primaryStage.setTitle("ShopLocal Website");
+        showMainView();
+
+        /*Parent root = FXMLLoader.load(getClass().getResource("AppScene.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setTitle("Title");
         primaryStage.setScene(scene);
-        primaryStage.show();
+        primaryStage.show();*/
+    }
+    private void showMainView() throws IOException{
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(AppScene.class.getResource("AppScene.fxml"));
+        mainLayout = loader.load();
+        Scene scene = new Scene(mainLayout);
+        primaryStage.setScene(scene);
+        primaryStage.show()
     }
 }
