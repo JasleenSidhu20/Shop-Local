1bf28e84406e74772be21e4ac67815af03b8d5e7
Jasleen <42920356+JasleenSidhu20@users.noreply.github.com>
2021-05-28 16:26:09 -0700

    added button

--- RegistrationController.java
+++ RegistrationController.java
@@ -2,6 +2,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert;
 
 public class RegistrationController {
 
@@ -34,5 +35,49 @@ public class RegistrationController {
 
     @FXML
     private Button registrationSubmitButton;
+
+    //@FXML
+    registrationSubmitButton.setOnAction(event -> {
+        try {
+            if (businessNameField.getText().isEmpty() || addressField.getText().isEmpty() || cityField.getText().isEmpty()
+                || stateField.getText().isEmpty() || zipcodeField.getText().equals("") || ownerNameField.getText().isEmpty()
+                || emailField.getText().isEmpty() || phoneNumberField.getText().equals("") ) {
+                Alert b = new Alert(Alert.AlertType.ERROR);
+                b.setTitle("ERROR");
+                b.setHeaderText("Make sure no fields are left empty!");
+                b.showAndWait();
+            } else if (Integer.parseInt(zipcodeField.getText()) < 0 || Integer.parseInt(phoneNumberField.getText()) < 0) {
+                //FIXME: Phone number less than length
+                Alert neg = new Alert(Alert.AlertType.ERROR);
+                neg.setTitle("ERROR");
+                neg.setHeaderText("Make sure you don't enter a negative number for questions 3, 4, 5, and 7!");
+                neg.showAndWait();
+            } else {
+                String businessName = businessNameField.getText();
+                String address = addressField.getText();
+                String city = cityField.getText();
+                String state = stateField.getText();
+                int zipcode = Integer.parseInt(zipcodeField.getText());
+                String ownerName = ownerNameField.getText();
+                //FIXME: Make owner name first and last name
+                String email = emailField.getText();
+                int phoneNumber = Integer.parseInt(phoneNumberField.getText());
+                businessNameField.clear();
+                addressField.clear();
+                cityField.clear();
+                stateField.clear();
+                zipcodeField.clear();
+                ownerNameField.clear();
+                emailField.clear();
+                phoneNumberField.clear();
+            }
+        } catch (NumberFormatException nfe) {
+            Alert a = new Alert(Alert.AlertType.ERROR);
+            a.setTitle("ERROR");
+            a.setHeaderText("Make sure you enter a number for questions 3, 4, 5 and 7!");
+            a.showAndWait();
+            //FIXME: Fix this 
+        }
+    });
 
 }
--- ShopLocal.java
+++ ShopLocal.java
@@ -7,33 +7,25 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.fxml.FXMLLoader;
-import java.io.IOException;
-
+//import javafx.scene.control.MenuItem;
 public class ShopLocal extends Application {
-    private Stage primaryStage;
-    private BorderPane mainLayout;
+    /*
+    @FXML
+    private MenuItem delete;
+
+    @FXML
+    void deleteProgram(ActionEvent event) {
+    }
+    */
     public static void main(String[] args){
         launch(args);
     }
-    
     @Override
     public void start(Stage primaryStage) throws Exception {
-        this.primaryStage = primaryStage;
-        this.primaryStage.setTitle("ShopLocal Website");
-        showMainView();
-
-        /*Parent root = FXMLLoader.load(getClass().getResource("AppScene.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("AppScene.fxml"));
         Scene scene = new Scene(root);
         primaryStage.setTitle("Title");
         primaryStage.setScene(scene);
-        primaryStage.show();*/
-    }
-    private void showMainView() throws IOException{
-        FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(AppScene.class.getResource("AppScene.fxml"));
-        mainLayout = loader.load();
-        Scene scene = new Scene(mainLayout);
-        primaryStage.setScene(scene);
-        primaryStage.show()
+        primaryStage.show();
     }
 }
