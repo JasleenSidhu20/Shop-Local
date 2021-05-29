4d27b477ed180243b98029dc4e909343149cbcc9
Serena Tzeng <serena.tzeng@gmail.com>
2021-05-29 00:08:18 -0500

    initial skeleton for controller

---
+++ AppSceneController.java
@@ -0,0 +1,36 @@
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Menu;
+import javafx.scene.control.TextField;
+
+public class PleaseProvideControllerClassName {
+
+    @FXML
+    private TextField location_input;
+
+    @FXML
+    private Button main_search_button;
+
+    @FXML
+    private ComboBox<?> type_of_business;
+
+    @FXML
+    private Menu register_business_button;
+
+    @FXML
+    void RegisterBusiness(ActionEvent event) {
+
+    }
+
+    @FXML
+    void SearchButton(ActionEvent event) {
+
+    }
+
+    @FXML
+    void TypeOfBusiness(ActionEvent event) {
+
+    }
+
+}
